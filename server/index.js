004aa65ff60088c887a02cec59e47e1954204064
dato_gogadze <dato.gogadze@quantori.com>
2022-04-14 20:36:20 +0400

    add migration support

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const db = require('./config/db');
+const db = require('./models/index');
 
 app.use(cors());
 
-db.authenticate()
-  .then(() => console.log('Database connected: ' + db.config.database))
+db.sequelize
+  .authenticate()
+  .then(() =>
+    console.log('Database connected: ' + db.sequelize.config.database)
+  )
   .catch(err => console.log('Database connection error: ' + err));
 
 const port = process.env.PORT || 3000;
---
+++ server/migrations/20220414162625-create-users-table.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
