4c104937a51b5374301ce64d18eeccdf4d66d1e9
dato_gogadze <dato.gogadze@quantori.com>
2022-04-14 17:53:33 +0400

    add database connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config();
+const db = require('./config/db');
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+db.authenticate()
+  .then(() => console.log('Database connected: ' + db.config.database))
+  .catch(err => console.log('Database connection error: ' + err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
