476166711a7730b93144c7ce522a0153535628c4
dato_gogadze <dato.gogadze@quantori.com>
2022-04-17 20:51:48 +0400

    google authorization

---
+++ client/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const HomeScreen = id => {
+  return (
+    <View>
+      <Text>Hello - {id}</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
