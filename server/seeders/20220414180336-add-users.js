681f687c594aecfb0fe594e64479f74d5456f5cb
dato_gogadze <dato.gogadze@quantori.com>
2022-04-23 19:18:38 +0400

    change seeder data

--- server/seeders/20220414180336-add-users.js
+++ server/seeders/20220414180336-add-users.js
@@ -10,14 +10,32 @@ module.exports = {
         {
           id: uuidv4(),
           email: 'johndoe1@mail.com',
+          email_verified: true,
+          first_name: 'John',
+          last_name: 'Doe',
+          display_name: 'John Doe',
+          password_hash:
+            '$2a$10$LHNg807mSDh38zFZJdvf0uY7WKZSo8lNVhFllDUhyV3.hOyn5rug.',
         },
         {
           id: uuidv4(),
           email: 'johndoe2@mail.com',
+          email_verified: true,
+          first_name: 'John',
+          last_name: 'Doe',
+          display_name: 'John Doe',
+          password_hash:
+            '$2a$10$LHNg807mSDh38zFZJdvf0uY7WKZSo8lNVhFllDUhyV3.hOyn5rug.',
         },
         {
           id: uuidv4(),
           email: 'johndoe3@mail.com',
+          email_verified: false,
+          first_name: 'John',
+          last_name: 'Doe',
+          display_name: 'John Doe',
+          password_hash:
+            '$2a$10$LHNg807mSDh38zFZJdvf0uY7WKZSo8lNVhFllDUhyV3.hOyn5rug.',
         },
       ],
       {}
