fb26d1e4a7ac5b23c3b536eecaa24865263de85d
datogogadze <gogadzedato@gmail.com>
2022-06-29 07:42:27 +0400

    set up test environment

--- server/app.js
+++ server/app.js
@@ -1,14 +1,12 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const db = require('./models/index');
 const passport = require('passport');
 const session = require('express-session');
 const flash = require('express-flash');
 const MySQLStore = require('express-mysql-session')(session);
+const MemoryStore = require('memorystore')(session);
 const mysql2 = require('mysql2/promise');
-const logger = require('./utils/winston');
 require('./utils/passport');
 
 // Routers
@@ -16,35 +14,27 @@ const auth = require('./routers/auth');
 const board = require('./routers/board');
 const user = require('./routers/user');
 const event = require('./routers/event');
-
-app.get('/register', (req, res) => {
-  res.render('register', {
-    layout: 'register',
-  });
-});
-app.get('/resetPassword', (req, res) => {
-  res.render('resetPassword', {
-    layout: 'resetPassword',
-  });
-});
+const e = require('express');
 
 const options = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database:
+    process.env.NODE_ENV == 'test'
+      ? process.env.TEST_DB_NAME
+      : process.env.DB_NAME,
 };
-var connection = mysql2.createPool(options);
-var sessionStore = new MySQLStore({}, connection);
 
-app.use(express.json());
-app.use(cors());
-app.use(flash());
+const connection = mysql2.createPool(options);
+const sessionStore = new MySQLStore({}, connection);
+const memoryStore = new MemoryStore();
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
-    store: sessionStore,
+    store: process.env.NODE_ENV == 'test' ? memoryStore : sessionStore,
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -52,24 +42,16 @@ app.use(
     },
   })
 );
+
+app.use(express.json());
+app.use(cors());
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-
-db.sequelize
-  .authenticate()
-  .then(() =>
-    logger.info('Database connected: ' + db.sequelize.config.database)
-  )
-  .catch((err) => logger.error('Database connection error: ' + err));
 
 app.use('/auth', auth);
 app.use('/board', board);
 app.use('/user', user);
 app.use('/event', event);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  logger.info(`Example app listening on port ${port}`);
-});
 
 module.exports = app;
