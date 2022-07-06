f8200cf2cd28c07a426bd8322f49f84437cd70e4
datogogadze <gogadzedato@gmail.com>
2022-07-06 19:26:45 +0400

    change async storage import

--- client/utils/auth.js
+++ client/utils/auth.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const setUser = async (user) => {
   const currentUser = await AsyncStorage.setItem('user', JSON.stringify(user));
